c0f614063dbabbd1246d75be97b4ec05561ff9ac
John Hearn <john.hearn@smadex.com>
2020-10-25 11:21:29 +0100

    Squeeze gates into same column if possible within chain

--- src/circuit-adapter.jl
+++ src/circuit-adapter.jl
@@ -8,13 +8,24 @@ function draw(chain::ChainBlock{n}) where n
 end
 
 function draw(xoffset, chain::ChainBlock{n}) where n
-    @show chain
-    xoff = xoffset
+    @debug chain
+    xoff = 0 # offset of current column
+    width = 0 # width of current column
+    occupied = Set() # set of occupied location in current column
     for block in chain.blocks
-        @show xoffset, xoff
-        xoff = draw(xoff, block)
+        occupies = Set(occupied_locs(block))
+        if (isdisjoint(occupies, occupied))
+            # No conflict so just draw in the same column
+            width = max(width, draw(xoffset + xoff, block))
+        else
+            # Overlaps with existing gate so move to next column
+            xoff += width
+            occupied = occupies
+            width = draw(xoffset + xoff, block)
+        end
+        union!(occupied, occupies)
     end
-    xoff
+    xoff + width # width of whole component
 end
 
 function name(const_gate::ConstantGate{1})
@@ -30,35 +41,35 @@ function name(shift_gate::ShiftGate{<:Real})
 end
 
 function draw(xoffset, row, const_gate::ConstantGate{1})
-    @show const_gate
+    @debug const_gate
     draw_gate(name(const_gate), xoffset, row)
-    xoffset + 50
+    50
 end
 
 function draw(xoffset, put_block::PutBlock{n,k,G}) where {n,k,G}
-    @show n,k,G,put_block.locs
+    @debug n,k,G,put_block.locs
     for i in put_block.locs # not sure about this
         draw(xoffset, i, put_block.content)
     end
-    xoffset + 50
+    50
 end
 
 function draw(xoffset, repeated_block::RepeatedBlock{n,k,G}) where {n,k,G}
-    @show n,k,G,repeated_block.locs
+    @debug n,k,G,repeated_block.locs
     for i in repeated_block.locs
         draw(xoffset, i, repeated_block.content)
     end
-    xoffset + 50
+    50
 end
 
 function draw(xoffset, control_block::ControlBlock{n,G,k,1}) where {n,G,k}
-    @show n,k,G,control_block.ctrl_locs,control_block.ctrl_config
+    @debug n,k,G,control_block.ctrl_locs,control_block.ctrl_config
     draw_control_gate(name(control_block.content), xoffset, control_block.locs[1], control_block.ctrl_locs, control_block.ctrl_config)
-    xoffset + 50
+    50
 end
 
 function draw(xoffset, matrix_block::GeneralMatrixBlock{M, n, MT}) where {M,n,MT}
-    @show matrix_block
+    @debug matrix_block
     draw_oracle("Uf", xoffset, 1, n)
-    xoffset + 60
+    60
 end
